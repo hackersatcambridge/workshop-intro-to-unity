12e371ffadb2e0ab58029ad03f4090b196e89540
Huw Bowles <huw.bowles@gmail.com>
2018-11-30 17:49:08 +0000

    Add static player

---
+++ UnityWorkshopGame/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	void Update ()
+    {
+        var mytransform = GetComponent<Transform>();
+        mytransform.position += Vector3.forward;
+	}
+}
