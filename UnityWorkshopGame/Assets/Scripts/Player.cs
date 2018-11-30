6c2fac6b8fd327326f32f7dfab206fe4f632e319
Huw Bowles <huw.bowles@gmail.com>
2018-11-30 18:18:54 +0000

    Jump initial version

--- UnityWorkshopGame/Assets/Scripts/Player.cs
+++ UnityWorkshopGame/Assets/Scripts/Player.cs
@@ -5,8 +5,40 @@ public class Player : MonoBehaviour
     [Range(0f, 60f)]
     public float _speed = 60f;
 
-	void Update ()
+    public Vector3 _jumpInitialVelocity = Vector3.up * 10f;
+
+    enum JumpState
+    {
+        NotJumping,
+        Jumping,
+    }
+    JumpState _jumpState = JumpState.NotJumping;
+
+    Vector3 _velocity = Vector3.zero;
+
+    void Update ()
     {
         transform.position += _speed * Vector3.forward * Time.deltaTime;
+
+        switch(_jumpState)
+        {
+            case JumpState.NotJumping:
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    // switch to jumping
+                    Debug.Log("Started jumping");
+
+                    _velocity = _jumpInitialVelocity;
+
+                    _jumpState = JumpState.Jumping;
+                }
+
+                break;
+            case JumpState.Jumping:
+                transform.position += _velocity * Time.deltaTime;
+                // 
+                break;
+        }
 	}
 }
